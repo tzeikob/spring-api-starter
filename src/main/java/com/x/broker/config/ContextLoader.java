cbd87c33fb7acf44c53bfe5999728f2d67e00a85
Akis Papadopoulos <iakopap@gmail.com>
2017-03-12 19:25:34 +0200

    Adding a basic application context loader.

---
+++ src/main/java/com/x/broker/config/ContextLoader.java
@@ -0,0 +1,48 @@
+package com.x.broker.config;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * An application context loader.
+ *
+ * @author Akis Papadopoulos
+ */
+public class ContextLoader extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        Class<?>[] classes = {};
+
+        return classes;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        Class<?>[] classes = {};
+
+        return classes;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[]{
+            new HiddenHttpMethodFilter()
+        };
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+
+        servletContext.setInitParameter("spring.profiles.default", "develop");
+    }
+}

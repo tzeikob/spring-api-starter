d25535b31e402cf7d8b1a5c39d322fa2538b873c
Akis Papadopoulos <iakopap@gmail.com>
2017-03-13 17:37:12 +0200

    Adding the RESTful resource controller for user entities.

---
+++ src/main/java/com/x/broker/rest/UserResource.java
@@ -0,0 +1,47 @@
+package com.x.broker.rest;
+
+import com.x.broker.data.UserRepository;
+import com.x.broker.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * A RESTful resource controller for user entities.
+ * 
+ * @author Akis Papadopoulos
+ */
+@RestController
+@RequestMapping("/user")
+public class UserResource {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping(value = "/{username}", method = RequestMethod.GET)
+    public ResponseEntity<User> getByUsername(@PathVariable String username) {
+        User user = userRepository.findByUsername(username);
+
+        if (user != null) {
+            return new ResponseEntity<User>(user, HttpStatus.FOUND);
+        } else {
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ResponseEntity<User> create(@RequestBody User user) {
+        try {
+            userRepository.save(user);
+        } catch (Exception exc) {
+            return new ResponseEntity<User>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<User>(user, HttpStatus.CREATED);
+    }
+}

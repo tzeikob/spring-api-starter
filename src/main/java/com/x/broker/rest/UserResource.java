491bb6cd32035350add3f2d4cfd8ef545255d1ad
Akis Papadopoulos <iakopap@gmail.com>
2017-03-13 20:29:52 +0200

    Adding CRUD methods using POST, PUT and DELETE verbs.

--- src/main/java/com/x/broker/rest/UserResource.java
+++ src/main/java/com/x/broker/rest/UserResource.java
@@ -2,7 +2,7 @@ package com.x.broker.rest;
 
 import com.x.broker.data.UserRepository;
 import com.x.broker.domain.User;
-import javax.ws.rs.core.MediaType;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,17 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * A RESTful resource controller for user entities.
- * 
+ *
  * @author Akis Papadopoulos
  */
 @RestController
-@RequestMapping(value = "/user",
-        produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML},
-        consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+@RequestMapping("/user")
 public class UserResource {
 
     @Autowired
     private UserRepository userRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<User>> get() {
+        List<User> list = userRepository.select();
+
+        if (list.isEmpty()) {
+            return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<List<User>>(list, HttpStatus.OK);
+        }
+    }
 
     @RequestMapping(value = "/{username}", method = RequestMethod.GET)
     public ResponseEntity<User> getByUsername(@PathVariable String username) {
@@ -37,7 +46,7 @@ public class UserResource {
         }
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<User> create(@RequestBody User user) {
         try {
             userRepository.save(user);
@@ -46,5 +55,29 @@ public class UserResource {
         }
 
         return new ResponseEntity<User>(user, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<User> update(@RequestBody User user) {
+        try {
+            userRepository.update(user);
+        } catch (Exception exc) {
+            return new ResponseEntity<User>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<User>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{username}", method = RequestMethod.DELETE)
+    public ResponseEntity<User> delete(@PathVariable String username) {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+
+        userRepository.delete(user);
+
+        return new ResponseEntity<User>(HttpStatus.OK);
     }
 }

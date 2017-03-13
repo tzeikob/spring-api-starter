03405dbb42f771ff2fae8abde811bb4c98f060cb
Akis Papadopoulos <iakopap@gmail.com>
2017-03-13 16:23:27 +0200

    Adding an abstract transactional DAO repository upon abstract entities.

---
+++ src/main/java/com/x/broker/data/AbstractRepository.java
@@ -0,0 +1,48 @@
+package com.x.broker.data;
+
+import com.x.broker.domain.AbstractEntity;
+import java.io.Serializable;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public abstract class AbstractRepository<T extends AbstractEntity> implements Serializable {
+
+    @Autowired
+    protected SessionFactory sessionFactory;
+
+    private Class<T> clazz;
+
+    public AbstractRepository(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    public List<T> select() {
+        List<T> list = (List<T>) sessionFactory.getCurrentSession()
+                .createCriteria(clazz)
+                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
+
+        return list;
+    }
+
+    public T find(long id) {
+        return (T) sessionFactory.getCurrentSession().get(clazz, id);
+    }
+
+    public void save(T entity) {
+        sessionFactory.getCurrentSession().save(entity);
+    }
+
+    public void update(T entity) {
+        sessionFactory.getCurrentSession().update(entity);
+    }
+
+    public void delete(T entity) {
+        sessionFactory.getCurrentSession().delete(entity);
+    }
+}

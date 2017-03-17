bd4392b207dc06f09ee435fb68abd7cd485518b0
Akis Papadopoulos <iakopap@gmail.com>
2017-03-17 15:42:55 +0200

    Throwing resource not found exception in update method.

--- src/main/java/com/x/broker/rest/UserResource.java
+++ src/main/java/com/x/broker/rest/UserResource.java
@@ -78,6 +78,10 @@ public class UserResource {
 
         User user = userRepository.find(id);
 
+        if (user == null) {
+            throw new UserNotFoundException(id);
+        }
+
         String username = data.getUsername();
 
         if (!username.equals(user.getUsername())
